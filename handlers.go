6392e6fe2f344ca6ab5ea495cc56b59ccac042d7
dan <dan.simko@gmail.com>
2017-04-08 20:03:28 +0200

    Added router

---
+++ handlers.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	r "github.com/GoRethink/gorethink"
+	"github.com/mitchellh/mapstructure"
+)
+
+func addChannel(client *Client, data interface{}) {
+	var channel Channel
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
+	go func() {
+		err := r.Table("channel").Insert(channel).Exec(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+}
--- main.go
+++ main.go
@@ -1,93 +1,31 @@
 package main
 
 import (
-	"fmt"
+	r "github.com/GoRethink/gorethink"
+	"log"
 	"net/http"
-	"github.com/gorilla/websocket"
-	"github.com/mitchellh/mapstructure"
-	"time"
 )
 
-type Message struct {
-	Name string `json:"name"`
-	Data interface{} `json:"data"`
-
-}
-
 type Channel struct {
-	Id string `json:"id"`
-	Name string `json:"name"`
+	Id   string `json:"id" gorethink:"id,omitempty"`
+	Name string `json:"name" gorethink: "name"`
 }
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
+type User struct {
+	Id   string `gorethink: "id,omitempty"`
+	Name string `gorethink: "name"`
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "rtsupport",
+	})
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	router := NewRouter(session)
+	router.Handle("channel add", addChannel)
+	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	socket, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		//msgType, msg, err := socket.ReadMessage()
-		//if err != nil {
-		//	fmt.Println(err)
-		//	return
-		//}
-
-		var inMessage Message
-		var outMessage Message
-		if err := socket.ReadJSON(&inMessage); err != nil {
-			fmt.Println(err)
-			break
-		}
-		fmt.Printf("%#v\n", inMessage)
-		switch inMessage.Name {
-		case "channel add":
-			err := addChannel(inMessage.Data)
-			if err != nil {
-				outMessage = Message{"error", err}
-				if err := socket.WriteJSON(outMessage); err != nil {
-					fmt.Println(err)
-					break
-				}
-			}
-		case "channel subscribe":
-			go subscribeChannel(socket)
-		}
-
-		//fmt.Println(string(msg))
-		//if err = socket.WriteMessage(msgType, msg); err != nil {
-		//	fmt.Println(err)
-		//	return
-		//}
-	}
-}
-
-func addChannel(data interface{}) (error){
-	var channel Channel
-	err := mapstructure.Decode(data, &channel)
-	if err != nil {
-		return err
-	}
-	channel.Id = "1"
-	fmt.Println("added channel")
-	return nil
-}
-
-func subscribeChannel(socket *websocket.Conn){
-	for{
-		time.Sleep(time.Second * 1)
-		message := Message{"channel add", Channel{"1", "Software Support"}}
-		socket.WriteJSON(message)
-		fmt.Println("sent new channel")
-	}
 }
